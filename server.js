ec213338d91deb0bbdb0826405a84034edaf01a1
Ariq Pradipa Santoso <ariq.pradipa@outlook.com>
2022-05-23 11:13:58 +0700

    create /createplayer route

--- server.js
+++ server.js
@@ -57,15 +57,14 @@ app.post("/register", (req, res) => {
     }
 
     const query = `INSERT INTO users VALUES ('${temp.username}', '${hashedPassword}');`;
-    db.query(query),
-      (err, results) => {
-        if (err) {
-          console.log(err);
-          return;
-        }
-        console.log(results);
-        console.log("Data insert berhasil");
-      };
+    db.query(query), (err, results) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(results.rows[0]);
+      console.log("Data insert berhasil");
+    };
   });
   res.end("user berhasil dibuat");
 });
@@ -93,6 +92,56 @@ app.post("/login", (req, res) => {
       res.end("Login");
     });
   });
+});
+
+app.post("/createplayer", (req, res) => {
+  const queryIdentitas = `INSERT INTO identitas(nama, umur, no_punggung, tinggi, berat_badan) VALUES ('${req.body.nama}', ${req.body.umur}, ${req.body.no_punggung}, ${req.body.tinggi}, '${req.body.berat_badan}');`;
+  const queryStatistik = `INSERT INTO statistik(agility, defence, shooting, passing, stamina, dribbling) VALUES (${req.body.agility}, ${req.body.defence}, ${req.body.shooting}, ${req.body.passing}, ${req.body.stamina}, ${req.body.dribbling});`;
+
+  db.query(queryIdentitas, (err, resultsIdentitas) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(resultsIdentitas);
+
+  });
+  db.query(queryStatistik, (err, resultsStatistik) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(resultsStatistik);
+  });
+
+  const queryGetIndentitas = `SELECT * FROM identitas ORDER BY id_identitas DESC LIMIT 1;`;
+  const queryGetStatistik = `SELECT * FROM statistik ORDER BY id_statistik DESC LIMIT 1;`;
+  db.query(queryGetIndentitas, (err, resultsGetIndentitas) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    console.log(resultsGetIndentitas);
+
+    db.query(queryGetStatistik, (err, resultsGetStatistik) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      console.log(resultsGetStatistik);
+
+      const querypemain = `INSERT INTO pemain(id_identitas, id_statistik, formasis) VALUES (${resultsGetIndentitas.rows[0].id_identitas}, ${resultsGetStatistik.rows[0].id_statistik}, '${req.body.formasis}');`;
+      db.query(querypemain, (err, resultsPemain) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(resultsPemain);
+      });
+    });
+  });
+  res.end("player created successfully");
 });
 
 app.post("/insert", (req, res) => {
